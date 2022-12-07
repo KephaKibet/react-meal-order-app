4d7aaa460603bbc7bb0fec3524daea1107bd26d4
Kepha Kibet <kibet.kepha@live.com>
2022-12-07 15:27:25 +0300

    using createPortal to create  reusable Modal component

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,3 +1,4 @@
+import Modal from '../UI/Modal';
 import classes from './Cart.module.css';
 
 const Cart = (props) => {
@@ -10,17 +11,17 @@ const Cart = (props) => {
   );
 
   return (
-    <div>
-      {cartItems}
-      <div className={classes.total}>
-        <span>Total Amount</span>
-        <span>35.62</span>
-      </div>
-      <div className={classes.actions}>
-        <button className={classes['button--alt']}>Close</button>
-        <button className={classes.button}>Order</button>
-      </div>
-    </div>
+    <Modal>
+        {cartItems}
+        <div className={classes.total}>
+          <span>Total Amount</span>
+          <span>35.62</span>
+        </div>
+        <div className={classes.actions}>
+          <button className={classes['button--alt']}>Close</button>
+          <button className={classes.button}>Order</button>
+        </div>
+      </Modal>
   );
 };
 
