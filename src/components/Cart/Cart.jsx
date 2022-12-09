4e5c22f9d74bc844350e7b3288bc28ba94a61517
Kepha Kibet <kibet.kepha@live.com>
2022-12-09 16:40:04 +0300

    style rendered cart & added add ang remove cart items handlers

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,19 +2,32 @@ import { useContext } from 'react';
 import Modal from '../UI/Modal';
 import classes from './Cart.module.css';
 import CartContext from '../../store/cart-context';
+import CartItem from './CartItem';
 
 const Cart = (props) => {
 
   const cartCtx = useContext(CartContext)
 
   const totalAmount = `${cartCtx.totalAmount.toFixed(2)}`
-  const hasItems= cartCtx.items.length > 0
-   =
+  const hasItems = cartCtx.items.length > 0;
+
+  const cartItemRemoveHandler = (id) => {};
+
+  const cartItemAddHandler = (item) => {};
+
 
   const cartItems = (
     <ul className={classes['cart-items']}>
       {cartCtx.items.map((item) => (
-        <li>{item.name}</li>
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          // bind ensures that the id of item to be added or removed is added to the handler function {pre-configures the function for future ecxecution}//
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.bind(null, item.id)}
+
+        />
       ))}
     </ul>
   );
