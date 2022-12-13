096d4bdcd1985b19eb07c4300b3148e776ae781d
Kepha Kibet <kibet.kepha@live.com>
2022-12-13 19:36:22 +0300

    Cart button bump on add items to cart

--- src/components/Layout/HeaderCartButton.jsx
+++ src/components/Layout/HeaderCartButton.jsx
@@ -1,17 +1,32 @@
-import { useContext } from 'react';
+import { useContext,useEffect,useState } from 'react';
 
 import CartIcon from '../Cart/CartIcon';
 import CartContext from '../../store/cart-context';
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
+
+  const[btnIsHighlighted,setbtnIsHighlighted]=useState(false)
   const cartCtx = useContext(CartContext);
 
-  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+  const { items } = cartCtx
+  
+  const numberOfCartItems = items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
 
-  const btnClasses =`${classes.button} ${classes.bump}`
+
+ 
+
+  const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`;
+
+  useEffect(() => {
+    if (items.leghth === 0)
+      return;
+    setbtnIsHighlighted(true)
+  }, [items]);
+  
+
 
   return (
     <button className={btnClasses} onClick={props.onClick}>
