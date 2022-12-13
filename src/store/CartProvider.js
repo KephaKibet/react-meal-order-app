55f3e7e61fbe57f55b6777be4d1f5636a10b08cd
Kepha Kibet <kibet.kepha@live.com>
2022-12-13 19:07:13 +0300

    bug fix- omitted strict equality operator ===

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -38,7 +38,7 @@ const cartReducer = (state, action) => {
   }
 
   if (action.type === 'REMOVE') {
-    const existingCartItemIndex = state.items.findIndex(item => item.id = action.item.id);
+    const existingCartItemIndex = state.items.findIndex(item => item.id === action.id);
 
     const existingItem = state.items[existingCartItemIndex];
     const updatedTotalAmount = state.totalAmount - existingItem.price;
