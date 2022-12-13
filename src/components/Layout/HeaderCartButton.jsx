208cf2ab8cbacd0fa7aebde973d90a92b563ef77
Kepha Kibet <kibet.kepha@live.com>
2022-12-13 19:20:25 +0300

    Cart button bump effect

--- src/components/Layout/HeaderCartButton.jsx
+++ src/components/Layout/HeaderCartButton.jsx
@@ -1,27 +1,3 @@
-// import { useContext } from 'react'
-
-// import CartContext from '../../store/cart-context'
-// import CartIcon from '../Cart/CartIcon'
-// import classes from './HeaderCartButton.module.css'
-
-
-// const HeaderCartButton = props => {
-//   const cartCtx = useContext(CartContext);
-//   const numberOfCartItems = cartCtx.items.reduce((acc, item) => {
-//     return acc + item.amount;
-//   }, 0)
-  
-//   return (
-//     <button className={classes.button} onClick={props.onClick}>
-//     <span className={classes.icon}> <CartIcon /> </span>
-//     <span>Your Cart</span>
-//       <span className={classes.badge}>{ numberOfCartItems}</span>
-//   </button>
-//   )
-// }
-
-// export default HeaderCartButton;
-
 import { useContext } from 'react';
 
 import CartIcon from '../Cart/CartIcon';
@@ -35,8 +11,10 @@ const HeaderCartButton = (props) => {
     return curNumber + item.amount;
   }, 0);
 
+  const btnClasses =`${classes.button} ${classes.bump}`
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
